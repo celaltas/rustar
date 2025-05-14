d983852699c09755b2ffaeafb8bb3033de5fbc64
celaltas <60509470+celaltas@users.noreply.github.com>
2025-05-14 20:07:53 +0300

    feat: create, list, extract and append commands are  implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,40 @@
 use archive::Archiver;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 mod archive;
 
 type TarResult<T> = Result<T, Box<dyn std::error::Error>>;
 
 #[derive(Parser)]
 #[command(version="1.0.0", about="RustyTar: A basic tar-like archiver rewritten in Rust", long_about = None)]
-pub struct Args {
-    #[arg(short, long)]
-    create: bool,
-    #[arg(long)]
-    delet: bool,
-    #[arg(short = 't', long)]
-    list: bool,
-    #[arg(short = 'x', long)]
-    extract: bool,
-    #[arg(short = 'r', long)]
-    append: bool,
-    #[arg(short, long, value_delimiter = ' ', num_args = 1..)]
-    files: Vec<String>,
+pub struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Create { archive: String, files: Vec<String> },
+    List { archive: String },
+    Append { archive: String, files: Vec<String> },
+    Extract { archive: String },
 }
 
 pub fn run() -> TarResult<()> {
-    let args = Args::parse();
+    let cli = Cli::parse();
     let archiver = Archiver::new();
-    if args.create {
-        match archiver.create("test.txt", args.files){
-            Ok(_) => println!("Archive created successfully."),
-            Err(e) => eprintln!("Error creating archive: {}", e),
+
+    match cli.command {
+        Command::Create { archive, files } => {
+            archiver.create(&archive, files)?;
+        }
+        Command::List { archive } => {
+            archiver.list(&archive)?;
+        }
+        Command::Append { archive, files } => {
+            archiver.append(&archive, files)?;
+        }
+        Command::Extract { archive } => {
+            archiver.extract(&archive)?;
         }
     }
     Ok(())
